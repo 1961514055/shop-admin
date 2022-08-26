fbbc47b4d3a73667c5e9163e764df50d427635f1
lxg <lxg9380@163.com>
2022-08-26 20:02:47 +0800

    平台管理页面--三级联动--表格数据展示完成

--- src/api/attr.ts
+++ src/api/attr.ts
@@ -1,16 +1,23 @@
 import request from '@/utils/request'
 
+export interface attrValueModel {
+  id: number,
+  attrId: number,
+  valueName: string
+}
+export interface attrInfoModel {
+  id: number,
+  attrName: string,
+  attrValueList: attrValueModel[]
+  categoryId: number | undefined,
+  categoryLevel: number,
+
+}
+export type attrInfoListModel = attrInfoModel[]
 
 export default {
-  //查询
+  // 查询列表数据--平台属性
   getAttrInfoList(category1Id: number, category2Id: number, category3Id: number) {
-    return request.get(`/admin/product/attrInfoList/${category1Id}/${category2Id}/${category3Id}`)
-  },
-  // 新增 修改
-  update() {
-    return request.post('/admin/product/saveAttrInfo')
-  },
-  del() {
-    return request.delete('/admin/product/deleteAttr')
+    return request.get<any, attrInfoListModel>(`/admin/product/attrInfoList/${category1Id}/${category2Id}/${category3Id}`)
   }
 }
--- src/views/product/attr/index.vue
+++ src/views/product/attr/index.vue
@@ -6,29 +6,61 @@
       </div>
     </el-card>
     <el-card class="box-card mt-10">
-      <div class="text item">属性信息</div>
+      <div class="text item">
+        <!-- 添加按钮 -->
+        <!-- <el-button type="primary" :icon="Plus" size="small" class="mb-10" :disabled="!categoryStore.category3Id ">添加属性 -->
+        <el-button type="primary" :icon="Plus" size="small" class="mb-10">添加属性
+        </el-button>
+        <!-- 列表表格 -->
+        <el-table border style="width: 100%" :data="attrList">
+          <el-table-column label="序号" width="80" type="index" />
+          <el-table-column prop="attrName" label="分类" width="160" />
+          <el-table-column prop="name" label="属性值列表">
+            <template #default="{ row, $index }">
+              <el-tag class="mr-10" type="success" v-for="attrVal in row.attrValueList">{{ attrVal.valueName }}</el-tag>
+            </template>
+          </el-table-column>
+          <el-table-column prop="name" label="操作" width="140">
+            <template #default="{ row, $index }">
+              <el-button type="warning" size="small" :icon="EditPen"></el-button>
+              <el-button type="danger" size="small" :icon="Delete"></el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
     </el-card>
   </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from "vue";
-/**
-分析:
-1. 三级联动 - 用组件去做(全局公用组件)
-  1.1 静态搭建
-  1.2 接口做好 - 去查看接口返回的数据,使用TS限制数据格式
-  1.3 数据展示 - 交互
-  1.4 当选择完3id的时候(如何让attr组件知道,3id有值了) - 有两个交互
-    按钮可用
-    获取主体内容列表展示数据
-2. 主体内容
-  2.1 把编辑和列表展示切换搞定 - 三级分类的禁用
-  ---------------------------------------------
-  2.2 准备api
-  2.2 列表展示页
-  2.3 新增/编辑页面
-*/
+import { ref, watch } from "vue";
+import { Plus, EditPen, Delete } from "@element-plus/icons-vue";
+import { useCategoryStore } from "@/stores/category";
+import type { attrInfoModel, attrInfoListModel } from '@/api/attr'
+import attrApi from '@/api/attr'
+// 三级分类仓库
+const categoryStore = useCategoryStore()
+// 平台属性  用来展示数据
+const attrList = ref<attrInfoListModel>()
+
+
+
+// 查询列表数据
+const getList = async () => {
+  const result = await attrApi.getAttrInfoList(categoryStore.category1Id as number, categoryStore.category2Id as number, categoryStore.category3Id as number)
+  attrList.value = result
+}
+
+
+// 监听三级分类 数据变化则调用查询接口
+watch(() => categoryStore.category3Id, (navl) => {
+  //  如果nval = und = false = 什么也没有  nval = dsa = true = 有值
+  if (!navl) {
+    attrList.value = []
+    return
+  }
+  getList()
+}, { immediate: true })
 
 
 
